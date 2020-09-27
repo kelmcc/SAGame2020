ba1ff9a0f23ce2b54f35a5f5e731f880d614dcd7
Kelly McCarter <35454029+KellyMAM@users.noreply.github.com>
2020-09-27 19:41:54 +0200

    Added a basic home screen

---
+++ Assets/HomeScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeScreenManager : MonoBehaviour
+{
+
+    public GameObject _root;
+
+    void Start()
+    {
+        _root.SetActive(true);
+    }
+
+    void Update()
+    {
+        //lol hacks
+        if (Input.anyKey)
+        {
+            _root.SetActive(false);
+        }
+    }
+}
