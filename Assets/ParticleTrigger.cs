ca5525942e8834015c88bf8eb3c108abd92da6c4
Kelly McCarter <35454029+KellyMAM@users.noreply.github.com>
2020-09-27 15:46:17 +0200

    Simple trigger for the brink

---
+++ Assets/ParticleTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem _particleSystem;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (!_particleSystem.isPlaying)
+            {
+                _particleSystem.Play();
+            }
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (_particleSystem.isPlaying)
+            {
+                _particleSystem.Stop();
+            }
+        }
+    }
+}
